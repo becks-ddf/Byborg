29faea0a6cdee92e777458c3963b2b6bf34d934d
dl <dl@payrow.com>
2024-08-15 23:19:59 -0500

    Added functions find_manual and update_manual_by_name to handle manual tags

--- tagmatch/vec_db.py
+++ tagmatch/vec_db.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import numpy as np
 from fastembed import TextEmbedding
@@ -7,7 +7,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.http.exceptions import UnexpectedResponse
 from qdrant_client.http.models import (CollectionInfo, Distance,
                                        FieldCondition, Filter, MatchValue,
-                                       PointStruct, VectorParams)
+                                       PointStruct, VectorParams, ScoredPoint)
 
 
 class Embedder:
@@ -31,10 +31,11 @@ class VecDB:
         self.distance = distance
         self.client = QdrantClient(host, port=int(port))
         self.collection = collection
+        self.manual_collection = "manual_" + self.collection
         self.vector_size = vector_size
 
         if not self.collection_exists():
-            self._create_collection
+            self._create_collection()
 
     def collection_exists(self) -> bool:
         try:
@@ -47,6 +48,7 @@ class VecDB:
         dist = Distance.COSINE if self.distance == "cosine" else Distance.EUCLID
         self.client.create_collection(self.collection,
                                       vectors_config=VectorParams(size=self.vector_size, distance=dist))
+        self.client.create_collection(self.manual_collection, vectors_config=VectorParams(size=self.vector_size, distance=dist))
 
     def remove_collection(self):
         self.client.delete_collection(self.collection)
@@ -58,11 +60,29 @@ class VecDB:
         res = self.client.search(self.collection, query_vector=vec_list, limit=k, query_filter=query_filter)
         return res
 
-    def store(self, vector: np.ndarray, payload: Dict[str, Any]) -> bool:
+    def find_manual(self, vector: np.ndarray, name: str) -> Optional[ScoredPoint]:
         vec_list: List[float] = vector.tolist()
-        rnd_id = uuid.uuid4().int & (1 << 64) - 1
+        query_filter = Filter(must=FieldCondition(key='name', match=MatchValue(value=name)))
+        res = self.client.search(self.manual_collection, query_vector=vec_list, query_filter=query_filter)
+        if res:
+            return res[0]
+        else:
+            return None
+
+    def update_manual_by_name(self, name: str, vector: np.ndarray, manual_tags: list):
+        res = self.find_manual(vector, name)
+        if res:
+            print(res.id)
+            self.store(vector, {"name": res.payload["name"], "manual": manual_tags}, res.id, True)
+        else:
+            self.store(vector=vector, payload={"name": name, "manual": manual_tags}, manual=True)
+
+    def store(self, vector: np.ndarray, payload: Dict[str, Any], idx: Optional[int] = None, manual: bool = False) -> bool:
+        vec_list: List[float] = vector.tolist()
+        rnd_id = idx if idx is not None else uuid.uuid4().int & (1 << 64) - 1
         try:
-            self.client.upsert(self.collection, points=[PointStruct(id=rnd_id, vector=vec_list, payload=payload)])
+            collection = self.manual_collection if manual else self.collection
+            self.client.upsert(collection, points=[PointStruct(id=rnd_id, vector=vec_list, payload=payload)])
             return True
         except Exception:
             return False
